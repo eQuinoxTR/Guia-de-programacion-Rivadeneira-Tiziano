860297b4960b5dd5316b6268de2dbdfb4b345e5d
eQuinoxTR <tiziano313rivadeneira@gmail.com>
2025-08-19 11:02:09 -0300

    terminado

--- (6) ejercicios for loops/ejercicio-49/main.c
+++ (6) ejercicios for loops/ejercicio-49/main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
 int main() {
-    
+    int minNum = 0, ultimoNum;
+    for (int i = 1; i<=5; i++){
+        printf("ingresa tu numero ");
+        scanf("%d", &ultimoNum);
+        if (i == 1 || ultimoNum < minNum) minNum = ultimoNum;
+    }
+    printf("el numero mas chico es %d ", minNum);
     return 0;
 }
