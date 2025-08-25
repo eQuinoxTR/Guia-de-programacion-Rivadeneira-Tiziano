cefc2445ebf446a54d46227860a803c28d0c85dc
eQuinoxTR <tiziano313rivadeneira@gmail.com>
2025-08-24 22:15:09 -0300

    ...

--- (1) ejercicios de secuencias/ejercicio-12/main.c
+++ (1) ejercicios de secuencias/ejercicio-12/main.c
@@ -1,10 +1,5 @@
 #include <stdio.h>
-#include <math.h> // Necesario para funciones como sqrt(), pow() y atan()
-
-// Definimos el valor de PI para la conversión a grados
-#ifndef M_PI
-#define M_PI 3.14159265358979323846
-#endif
+#include <math.h>
 
 int main() {
     double R, L, C, w;
@@ -22,11 +17,10 @@ int main() {
 
     double term_admitancia = pow(1.0 / R, 2) + pow((w * C - 1.0 / (w * L)), 2);
     Z = 1.0 / sqrt(term_admitancia);
-
     phi = -atan((w * C - 1.0 / (w * L)) * R);
 
     printf("Módulo de la impedancia Z: %.4f Ohms\n", Z);
     printf("Ángulo de la impedancia: %f radianes\n", phi);
 
-    return 0; // El programa finaliza con éxito
+    return 0;
 }
